bb0da99e1f8ea58ebb29c1cbce5f92b474eeca21
HypoMarkup <163661923+HypoMarkup@users.noreply.github.com>
2026-07-21 20:47:33 +0100

    context nvim

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  event = "BufReadPost",
+  opts = {
+    max_lines = 3,           -- max number of context lines to show
+    min_window_height = 20,  -- disable in small windows
+    mode = "cursor",         -- "cursor" or "topline"
+    separator = nil,         -- set to "─" for a separator line
+    multiline_threshold = 1, -- prevents { under the func using a line
+  },
+  keys = {
+    { "[c", function() require("treesitter-context").go_to_context() end, desc = "Jump to context" },
+  },
+}
