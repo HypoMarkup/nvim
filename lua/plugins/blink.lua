3d9377658167713f6f0b0146d3b2a4f52442285e
HypoMarkup <163661923+HypoMarkup@users.noreply.github.com>
2026-08-01 18:19:42 +0100

    prevent blink consuming esc

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -33,7 +33,7 @@ return
     keymap = {
       preset = 'super-tab',
       ["<CR>"] = { "accept", "fallback" },
-      ["<Esc>"] = { "hide", "fallback" },
+      ["<C-l>"] = { "hide", "fallback" }, -- ctrl e also does this because of the preset
       ["<C-d>"] = { "show_documentation", "hide_documentation", "fallback" },
       ["<C-b>"] = { "scroll_documentation_up", "fallback" },
       ["<C-f>"] = { "scroll_documentation_down", "fallback" },
